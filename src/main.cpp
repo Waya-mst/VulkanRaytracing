46f6e2eccc5e1e33b111436fc7132eedf51c6df0
Waya-mst <kameeeeei1234@gmail.com>
2024-10-10 14:49:56 +0900

    implemented create window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,41 @@
-#include <iostream>
+#include "vkutils.hpp"
+
+constexpr uint32_t width = 800;
+constexpr uint32_t height = 600;
+
+class Application
+{
+public:
+	void run() {
+		initWindow();
+		initVulkan();
+
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
+
+		glfwDestroyWindow(window);
+		glfwTerminate();
+	}
+
+private:
+	
+	GLFWwindow* window = nullptr;
+
+	void initWindow() {
+		glfwInit();
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		window = glfwCreateWindow(width, height, "vulkanRaytracing", nullptr, nullptr);
+	}
+
+	void initVulkan() {
+
+	}
+};
 
 int main() {
-	std::cout << "hello" << std::endl;
+	Application app;
+	app.run();
 	return 0;
 }
