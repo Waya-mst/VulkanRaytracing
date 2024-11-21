d49a65f3586fb018a940921e60c55fb2941a4eb5
Waya-mst <kameeeeei1234@gmail.com>
2024-11-22 02:17:09 +0900

    implemented create SBT

--- src/main.cpp
+++ src/main.cpp
@@ -165,6 +165,11 @@ private:
 	vk::UniquePipeline pipeline;
 	vk::UniquePipelineLayout pipelineLayout;
 
+	Buffer sbt{};
+	vk::StridedDeviceAddressRegionKHR raygenRegion{};
+	vk::StridedDeviceAddressRegionKHR missRegion{};
+	vk::StridedDeviceAddressRegionKHR hitRegion{};
+
 	void initWindow() {
 		glfwInit();
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
@@ -222,6 +227,8 @@ private:
 		createDescriptorSet();
 
 		createRayTracingPipeline();
+
+		createShaderBindingTable();
 	}
 
 	void createSwapchainImageViews() {
@@ -462,6 +469,72 @@ private:
 		}
 
 		pipeline = std::move(result.value);
+	}
+
+	void createShaderBindingTable() {
+		vk::PhysicalDeviceRayTracingPipelinePropertiesKHR rtProperties = 
+			vkutils::getRayTracingProps(physicalDevice);
+		uint32_t handleSize = rtProperties.shaderGroupHandleSize;
+		uint32_t handleAlignment = rtProperties.shaderGroupHandleAlignment;
+		uint32_t baseAlignment = rtProperties.shaderGroupBaseAlignment;
+		uint32_t handleSizeAligned = vkutils::alignUp(handleSize, handleAlignment);
+
+		// Set strides and sizes
+		uint32_t raygenShaderCount = 1;  // raygen count must be 1
+		uint32_t missShaderCount = 1;
+		uint32_t hitShaderCount = 1;
+
+		raygenRegion.setStride(vkutils::alignUp(handleSizeAligned, baseAlignment));
+		raygenRegion.setSize(raygenRegion.stride);
+
+		missRegion.setStride(handleSizeAligned);
+		missRegion.setSize(vkutils::alignUp(missShaderCount * handleSizeAligned, baseAlignment));
+
+		hitRegion.setStride(handleSizeAligned);
+		hitRegion.setSize(vkutils::alignUp(hitShaderCount * handleSizeAligned, baseAlignment));
+
+		vk::DeviceSize sbtSize = raygenRegion.size + missRegion.size + hitRegion.size;
+		sbt.init(physicalDevice, *device, sbtSize,
+			vk::BufferUsageFlagBits::eShaderBindingTableKHR |
+			vk::BufferUsageFlagBits::eTransferSrc |
+			vk::BufferUsageFlagBits::eShaderDeviceAddress,
+			vk::MemoryPropertyFlagBits::eHostVisible |
+			vk::MemoryPropertyFlagBits::eHostCoherent);
+
+		uint32_t handleCount = raygenShaderCount + missShaderCount + hitShaderCount;
+		uint32_t handleStorageSize = handleCount * handleSize;
+		std::vector<uint8_t> handleStorage(handleStorageSize);
+		auto result = device->getRayTracingShaderGroupHandlesKHR(
+			*pipeline, 0, handleCount, handleStorageSize, handleStorage.data());
+		if (result != vk::Result::eSuccess) {
+			std::cerr << "Failed to get ray tracing shader group handles.\n";
+			std::abort();
+		}
+
+		uint8_t* sbtHead = static_cast<uint8_t*>(device->mapMemory(*sbt.memory, 0, sbtSize));
+		uint8_t* dstPtr = sbtHead;
+		auto copyHandle = [&](uint32_t index) {
+			std::memcpy(dstPtr, handleStorage.data() + handleSize * index, handleSize);
+		};
+
+		uint32_t handleIndex = 0;
+		copyHandle(handleIndex++);
+
+		dstPtr = sbtHead + raygenRegion.size;
+		for (uint32_t c = 0; c < missShaderCount; c++) {
+			copyHandle(handleIndex++);
+			dstPtr += missRegion.stride;
+		}
+
+		dstPtr = sbtHead + raygenRegion.size + missRegion.size;
+		for (uint32_t c = 0; c < hitShaderCount; c++) {
+			copyHandle(handleIndex++);
+			dstPtr += hitRegion.stride;
+		}
+
+		raygenRegion.setDeviceAddress(sbt.address);
+		missRegion.setDeviceAddress(sbt.address + raygenRegion.size);
+		hitRegion.setDeviceAddress(sbt.address + raygenRegion.size + missRegion.size);
 	}
 };
 
